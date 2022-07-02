0fe8699949c8bc65076b7d5c80842742918c173d
Rami <rami.zayat9@gmail.com>
2022-07-02 23:08:24 +0200

    adde mpu9250

--- main/main.cpp
+++ main/main.cpp
@@ -12,7 +12,7 @@
 #include "esp_log.h"
 #include "sdkconfig.h"
 #include "Arduino.h"
-
+#include "nvs_flash.h" 
 #include "SHT3x.h"
 #include "BLEDescriptor.h"
 #include "BLECharacteristic.h"
@@ -21,18 +21,27 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <Wire.h>
+#include "MPU9250.h"
+#include <SparkFunMPU9250-DMP.h>
 
+MPU9250_DMP imu;
 //BLE server name
 #define bleServerName "BME280_ESP32"
 
+unsigned long stepCount = 0;
+unsigned long stepTime = 0;
+unsigned long lastStepCount = 0;
 float temp;
 float tempF;
 float hum;
-
+#define SDA_PIN 27
+#define SCLK_PIN 26
 // Timer variables
 unsigned long lastTime = 0;
-unsigned long timerDelay = 30000;
-SHT3x shtTempSensor;
+unsigned long timerDelay = 3000;
+SHT3x shtTempSensor = SHT3x();
+MPU9250 mpu = MPU9250();
+
 bool deviceConnected = false;
 
 // See the following for generating UUIDs:
@@ -58,10 +67,12 @@ class MyServerCallbacks : public BLEServerCallbacks {
 };
 
 void initBME() {
+    Wire.begin(SDA_PIN, SCLK_PIN);
     shtTempSensor.Begin();
 }
 
 void setup() {
+    nvs_flash_init();
     // Start serial communication 
     Serial.begin(115200);
     // Init BME Sensor
@@ -85,7 +96,7 @@ void setup() {
     // Humidity
     bmeService->addCharacteristic(&bmeHumidityCharacteristics);
     bmeHumidityDescriptor.setValue("BME humidity");
-    bmeHumidityCharacteristics.addDescriptor(new BLE2902());
+    bmeHumidityCharacteristics.addDescriptor(&bmeHumidityDescriptor);
 
     // Start the service
     bmeService->start();
@@ -95,36 +106,73 @@ void setup() {
     pAdvertising->addServiceUUID(SERVICE_UUID);
     pServer->getAdvertising()->start();
     Serial.println("Waiting a client connection to notify...");
+    if (!mpu.setup(0x68)) {  // change to your own address
+        while (1) {
+            Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+            delay(5000);
+        }
+    }
+    else
+    {
+        mpu.calibrateAccelGyro();
+        delay(5000);
+        mpu.calibrateMag();
+        delay(5000);
+    }
+    if (imu.begin() != INV_SUCCESS)
+    {
+        while (1)
+        {
+            Serial.println("Check connections, and try again.");
+            delay(5000);
+        }
+    }
+
+    else
+    {
+        imu.dmpBegin(DMP_FEATURE_PEDOMETER);
+        imu.dmpSetPedometerSteps(stepCount);
+        imu.dmpSetPedometerTime(stepTime);
+    }
 }
 
+void print_roll_pitch_yaw() {
+    Serial.print("Yaw, Pitch, Roll: ");
+    Serial.print(mpu.getYaw(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getPitch(), 2);
+    Serial.print(", ");
+    Serial.println(mpu.getRoll(), 2);
+}
+
+
 void loop() {
-    if (deviceConnected) {
-        if ((millis() - lastTime) > timerDelay)
-        {
-            // Read temperature as Celsius (the default)
-            temp = shtTempSensor.GetTemperature();
-            // Read humidity
-            hum = shtTempSensor.GetRelHumidity();
-            //Notify temperature reading from BME sensor
-            static char temperatureCTemp[6];
-            dtostrf(temp, 6, 2, temperatureCTemp);
-            //Set temperature Characteristic value and notify connected client
-            bmeTemperatureCelsiusCharacteristics.setValue(temperatureCTemp);
-            bmeTemperatureCelsiusCharacteristics.notify();
-            Serial.print("Temperature Celsius: ");
-            Serial.print(temp);
-            Serial.print(" ºC");
-            //Notify humidity reading from BME
-            static char humidityTemp[6];
-            dtostrf(hum, 6, 2, humidityTemp);
-            //Set humidity Characteristic value and notify connected client
-            bmeHumidityCharacteristics.setValue(humidityTemp);
-            bmeHumidityCharacteristics.notify();
-            Serial.print(" - Humidity: ");
-            Serial.print(hum);
-            Serial.println(" %");
-            lastTime = millis();
-        }
+    if ((millis() - lastTime) > timerDelay)
+    {
+        // Read temperature as Celsius (the default)
+        shtTempSensor.UpdateData();
+        temp = shtTempSensor.GetTemperature();
+        // Read humidity
+        hum = shtTempSensor.GetRelHumidity();
+        //Notify temperature reading from BME sensor
+        static char temperatureCTemp[6];
+        dtostrf(temp, 6, 2, temperatureCTemp);
+        //Set temperature Characteristic value and notify connected client
+        bmeTemperatureCelsiusCharacteristics.setValue(temperatureCTemp);
+        bmeTemperatureCelsiusCharacteristics.notify();
+        Serial.print("Temperature Celsius: ");
+        Serial.print(temp);
+        Serial.print(" ºC");
+        //Notify humidity reading from BME
+        static char humidityTemp[6];
+        dtostrf(hum, 6, 2, humidityTemp);
+        //Set humidity Characteristic value and notify connected client
+        bmeHumidityCharacteristics.setValue(humidityTemp);
+        bmeHumidityCharacteristics.notify();
+        Serial.print(" - Humidity: ");
+        Serial.print(hum);
+        Serial.println(" %");
+        lastTime = millis();
     }
 }
 
@@ -132,6 +180,13 @@ extern "C" void app_main(void)
 {
     setup();
     while (1) {
-       loop();
+        /*if (mpu.update()) {
+            static uint32_t prev_ms = millis();
+            if (millis() > prev_ms + 25) {
+                print_roll_pitch_yaw();
+                prev_ms = millis();
+            }
+        }*/
+        loop();
     }
 }
