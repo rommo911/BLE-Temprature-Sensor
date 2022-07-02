3e8f656fe5f435cadf1fd027edb40f7d85cc8ee2
Rami <rami.zayat9@gmail.com>
2022-07-02 13:20:38 +0200

    Init

---
+++ main/main.cpp
@@ -0,0 +1,21 @@
+/* Blink Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+#include "Arduino.h"
+
+extern "C" void app_main(void)
+{
+    while (1) {
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+}
